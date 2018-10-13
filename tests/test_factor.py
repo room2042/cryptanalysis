40c303f355d252cd942e35ee2e09d91677f88033
Tim van de Kamp <6726197+timothymctim@users.noreply.github.com>
2018-10-13 16:32:14 +0200

    Add Fermat factoring

--- tests/test_factor.py
+++ tests/test_factor.py
@@ -30,6 +30,46 @@ class FactorTestCase(unittest.TestCase):
     def test_phi(self):
         self.assertEqual(1440394655298318578075682315282432000, self.factor.phi)
 
+class FermatTestCase(unittest.TestCase):
+    def test_fermat(self):
+        # test even n
+        n = 2*17*19
+        factor = Factor(n)
+        factor.fermat()
+
+        self.assertIn(2, factor.factors)
+        self.assertIn(17, factor.factors)
+        self.assertIn(19, factor.factors)
+
+        # test perfect square
+        n = 15*15
+        factor = Factor(n)
+        factor.fermat()
+
+        self.assertIn(3, factor.factors)
+        self.assertIn(5, factor.factors)
+
+        # test ratio
+        p = 670782728334373
+        q = 1341565456668793
+        factor = Factor(p * q)
+        factor.fermat((1, 2))
+
+        self.assertIn(p, factor.factors)
+        self.assertIn(q, factor.factors)
+
+        # test finished factoring
+        factor.fermat((1, 1))
+
+    def test_RSA_fermat(self):
+        p = 11326943005628119672694629821649856331564947811949928186125208046290130000912216246378177299696220728414241927034282796937320547048361486068608744598351187
+        q = 11326943005628119672694629821649856331564947811949928186125208046290130000912120768861173564277210907403841603312764378561200102283658817695884193223692869
+        factor = Factor(p * q)
+        factor.fermat()
+
+        self.assertIn(p, factor.factors)
+        self.assertIn(q, factor.factors)
+
 class SmoothTestCase(unittest.TestCase):
     def setUp(self):
         self.factors = {
